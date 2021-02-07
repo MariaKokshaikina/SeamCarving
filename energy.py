88a6bd04d65446d83b96f380e2c0fc097abd2452
Olexandr Yermilov <olexandr.yermilov@gmail.com>
2021-02-07 02:58:26 +0200

    Make gradient energy more efficient

--- energy.py
+++ energy.py
@@ -7,24 +7,38 @@ def color_to_gray(img):
     return (img[:, :, 0] + img[:, :, 1] + img[:, :, 2]) / 3.0
 
 
-def energy_gradient(img, importance_map):
+def energy_gradient_for_i_j(img, i, j, importance_map):
     height = img.shape[0]
     width = img.shape[1]
-    energy = np.empty((height, width))
-    for i in range(height):
-        for j in range(width):
-            L = img[i, (j - 1) % width]
-            R = img[i, (j + 1) % width]
-            U = img[(i - 1) % height, j]
-            D = img[(i + 1) % height, j]
+    L = img[i, (j - 1) % width]
+    R = img[i, (j + 1) % width]
+    U = img[(i - 1) % height, j]
+    D = img[(i + 1) % height, j]
 
-            dx_sq = np.sum((R - L) ** 2)
-            dy_sq = np.sum((D - U) ** 2)
-            energy[i, j] = np.sqrt(dx_sq + dy_sq) + importance_map[i, j] * IMPORTANCE_COEF
+    dx_sq = np.sum((R - L) ** 2)
+    dy_sq = np.sum((D - U) ** 2)
+    return np.sqrt(dx_sq + dy_sq) + importance_map[i, j] * IMPORTANCE_COEF
+
+def energy_gradient(img, importance_map, mask, old_energy):
+    height = img.shape[0]
+    width = img.shape[1]
+    if(old_energy is None):
+        energy = np.empty((height, width))
+    else:
+        energy = old_energy
+    if(not mask is None):
+        indices = np.where(mask[:,:,0] == False)
+        for i in range(len(indices[1])):
+            for j in range(max(0,indices[1][i]-2), min(indices[1][i]+2, width)):
+                energy[i,j] = energy_gradient_for_i_j(img, i, j, importance_map)
+    else:
+        for i in range(height):
+            for j in range(width):
+                energy[i,j] = energy_gradient_for_i_j(img, i, j, importance_map)
     return energy
 
 
-def gradient_magnitude_sobel_operator(img, importance_map):
+def gradient_magnitude_sobel_operator(img, importance_map, mask, old_energy):
     image = color_to_gray(img)
     filter = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
     new_image_x = signal.convolve2d(image, filter)[1:-1,1:-1]
@@ -34,7 +48,6 @@ def gradient_magnitude_sobel_operator(img, importance_map):
 
     height = image.shape[0]
     width = image.shape[1]
-    energy = np.empty((height, width))
     for i in range(height):
         for j in range(width):
             gradient_magnitude[i, j] += importance_map[i, j] * IMPORTANCE_COEF
@@ -43,5 +56,5 @@ def gradient_magnitude_sobel_operator(img, importance_map):
     return gradient_magnitude
 
 
-def energy_function_forward(img,  importance_map):
+def energy_function_forward(img,  importance_map,  mask, old_energy):
     return None
--- seam_carve.py
+++ seam_carve.py
@@ -11,14 +11,15 @@ IMAGES_DIR = 'images'
 
 
 class SeamCarve:
-    
+
     def __init__(self,
                  image_path,
                  new_width,
                  importance_map,
                  energy_function,
                  seam_map_function,
-                 carve_function):
+                 carve_function,
+                 name_suffix):
         
         self.initial_image = Image.open(image_path)
         self.image_name = os.path.splitext(os.path.basename(image_path))[0]
@@ -30,7 +31,9 @@ class SeamCarve:
         self.energy_function = energy_function
         self.seam_map_function = seam_map_function
         self.carve_function = carve_function
-        
+
+        self.name_suffix = name_suffix
+
         self.time_by_step = []
         self.final_image = None
         
@@ -40,50 +43,55 @@ class SeamCarve:
     def run(self):
         
         img = np.asarray(self.initial_image)
-        
+
+        energy = None
+        mask = None
+
         width_diff = self.original_width - self.new_width
-        
+
         if width_diff >= 0:
-        
+
             for i in tqdm(range(self.new_width, self.original_width)):
-                
+
                 img_gif = img.copy()
                 r,c,_ = img.shape
 
                 start_time = time.clock()
-                energy = self.energy_function(img, self.importance_map)
+                energy = self.energy_function(img, self.importance_map, mask, energy)
                 map_, backtrack = self.seam_map_function(img, energy)
                 mask = self.carve_function(img, map_, backtrack)
                 mask = np.stack([mask] * 3, axis=2)
-                
+                energy = energy[mask[:, :, 0]].reshape((r, c - 1))
+                self.importance_map = self.importance_map[mask[:, :, 0]].reshape((r, c - 1))
                 img = img[mask].reshape((r, c - 1, 3))
                 self.time_by_step.append(time.clock() - start_time)
 
                 img_gif[mask[:,:,0]==False] = (255,0,0)
                 self.images_for_gif.extend([img_gif, img])
 
-            self.final_image = Image.fromarray(img)                
-        
+            self.final_image = Image.fromarray(img)
+
         elif width_diff < 0:
-            
+
             img_tmp = img.copy()
             masks = []
-            
+
             gif0 = np.empty((img_tmp.shape[0], np.abs(width_diff), 3), dtype=np.uint8)
             gif0.fill(255)
             gif0 = np.concatenate((img_tmp, gif0), axis=1)
             self.images_for_gif.append(gif0)
-            
-            
+
+
             for i in tqdm(range(2 * self.original_width - self.new_width, self.original_width)):
 
                 r,c,_ = img_tmp.shape
-                
-                energy = self.energy_function(img_tmp, self.importance_map)
+
+                energy = self.energy_function(img_tmp, self.importance_map, mask, energy)
                 map_, backtrack = self.seam_map_function(img_tmp, energy)
                 mask = self.carve_function(img_tmp, map_, backtrack)
                 masks.append(mask)
-
+                energy = energy[mask[:, :, 0]].reshape((r, c - 1))
+                self.importance_map = self.importance_map[mask[:, :, 0]].reshape((r, c - 1))
                 mask = np.stack([mask] * 3, axis=2)
                 img_tmp = img_tmp[mask].reshape((r, c - 1, 3))
 
@@ -98,12 +106,12 @@ class SeamCarve:
                 self.images_for_gif.extend([img_gif, img])
 
                 masks = self.update_mask(current_mask, masks)
-            
+
             self.final_image = Image.fromarray(img)
-    
-    
+
+
     def add_seam_to_img(self, img, mask):
-    
+
         initial_img = img.copy()
         r,c,g = initial_img.shape
         new_img = np.zeros((r, c+1, g))
@@ -121,10 +129,10 @@ class SeamCarve:
                     new_img[row, col + 1:, rgb] = initial_img[row, col:, rgb]
 
         return new_img.astype(np.uint8)
-    
-    
+
+
     def update_mask(self, current_mask, masks_left):
-    
+
         new_masks = []
 
         curr_mask_col = np.where(current_mask == False)[1]
@@ -142,23 +150,31 @@ class SeamCarve:
             new_masks.append(new_mask)
 
         return new_masks
-    
-    
+
+
     def save_result(self, name_suffix, gif_fps=10):
-        
+
         if not os.path.exists(IMAGES_DIR):
                 os.mkdir(IMAGES_DIR)
                 
         self.final_image.save(
             f'{IMAGES_DIR}/{self.image_name}_'
             f'w{self.original_width}to{self.new_width}'
-            f'_{name_suffix}.jpg')
+            f'_{self.name_suffix}.jpg')
         
         if not os.path.exists(GIFS_DIR):
             os.mkdir(GIFS_DIR)  
             
         imageio.mimwrite(f'{GIFS_DIR}/{self.image_name}'
                          f'_w{self.original_width}to{self.new_width}'
-                         f'_{name_suffix}.gif', 
+                         f'_{self.name_suffix}.gif',
                          self.images_for_gif, fps=gif_fps)
-        
+
+    def show_gif(self):
+        import base64
+        from IPython import display
+        with open(f'{GIFS_DIR}/{self.image_name}'
+                  f'_w{self.original_width}to{self.new_width}'
+                  f'_{self.name_suffix}.gif', 'rb') as fd:
+            b64 = base64.b64encode(fd.read()).decode('ascii')
+        return display.HTML(f'<img src="data:image/gif;base64,{b64}" width="300" />')
